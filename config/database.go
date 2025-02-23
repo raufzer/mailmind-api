61fa6618af50a19e856751e6e115bb778abce1f0
raufzer <a_zerkhef@estin.dz>
2025-02-23 19:09:16 +0100

    refactor: remove Redis connection code for streamlined database configuration

--- config/database.go
+++ config/database.go
@@ -4,37 +4,14 @@ import (
 	"context"
 	"log"
 
-	"github.com/go-redis/redis/v8"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-type RedisConfig struct {
-	Client *redis.Client
-}
 
 
-func ConnectRedis(config *AppConfig) *RedisConfig {
 
-	client := redis.NewClient(&redis.Options{
-		Addr:     config.RedisURI,
-		Password: config.RedisPassword,
-		DB:       0,
-	})
-
-	ctx := context.Background()
-
-	if err := client.Ping(ctx).Err(); err != nil {
-		log.Fatalf("Error while connecting to Redis: %v", err)
-	}
-
-	log.Println("Connected to Redis successfully")
-
-	return &RedisConfig{
-		Client: client,
-	}
-}
 type DatabaseConfig struct {
 	Client         *mongo.Client
 	UserCollection *mongo.Collection
