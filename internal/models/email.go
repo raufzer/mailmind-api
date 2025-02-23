6dc7e7e2495607d13471b898dcc990572b515530
raufzer <a_zerkhef@estin.dz>
2025-02-23 19:10:06 +0100

    refactor: remove unused UserID and EmailID fields from Email model

--- internal/models/email.go
+++ internal/models/email.go
@@ -4,13 +4,10 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
-
 )
 
 type Email struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty"`
-	UserID     primitive.ObjectID `bson:"user_id"`
-	EmailID    primitive.ObjectID `bson:"email_id"`
 	Sender     string             `bson:"sender"`
 	Recipient  string             `bson:"recipient"`
 	Subject    string             `bson:"subject"`
